01af4d4c014a2ab1cace0004b0b3eb5e28d68e15
Murat DAL <murat90452@ogr.duzce.edu.tr>
2022-04-26 17:15:50 +0300

    murat220426

---
+++ HospitalSystemProgram/KullaniciGirisi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace HospitalSystemProgram
+{
+    public partial class KullaniciGirisi : Form
+    {
+        public KullaniciGirisi()
+        {
+            InitializeComponent();
+        }
+
+        private void KullaniciGirisi_Load(object sender, EventArgs e)
+        {
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = "Data Source=(DESCRIPTION = " +
+"(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))" +
+"(CONNECT_DATA =" +
+ "(SERVER = DEDICATED)" +
+  "(SERVICE_NAME = oracle.murat.com)" +
+")" +
+");User Id = MURAT;password=dal;";
+            con.Open();
+        }
+    }
+}
